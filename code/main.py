ae89c2fd48cf80b8d97a801f55557b514be8fb01
Heber Scachetti <heber.augusto@gmail.com>
2024-01-09 00:14:32 -0300

    Update main.py

--- code/main.py
+++ code/main.py
@@ -11,6 +11,8 @@ storage_client = storage.Client.from_service_account_json(google_storage_json_fi
 bucket_id = os.getenv('GCP_BUCKET', '')
 
 gs_prefix = os.getenv('GCP_PREFIX', '')
+dest_folder_id = os.getenv('DEST_FOLDER_ID', '')
+team_drive_id = os.getenv('TEAM_DRIVER_ID', '')
 
 
 bucket_id = 'observatorio-oncologia'
@@ -37,5 +39,62 @@ def autenticar_servico(json_caminho, escopos):
     credenciais = service_account.Credentials.from_service_account_file(json_caminho, scopes=escopos)
     return build('drive', 'v3', credentials=credenciais)
 
+def obter_dados_pasta(servico, team_drive_id, pasta_id, current_path=''):
+    quantidade_arquivos = 0
+    tamanho_total = 0
+    lista_arquivos = []
+    pagina_token = None
+
+    while True:
+        # Listar os arquivos na pasta
+        resultados = servico.files().list(
+            q=f"'{pasta_id}' in parents",
+            fields="*",
+            driveId=team_drive_id,
+            includeItemsFromAllDrives=True,
+            supportsAllDrives=True,
+            corpora='drive',
+            pageToken=pagina_token
+            ).execute()
+        arquivos = resultados.get('files', [])
+
+
+        for arquivo in arquivos:
+            if (arquivo['mimeType'] != 'application/vnd.google-apps.folder'):
+                if int(arquivo['quotaBytesUsed']) > 0:
+                    tamanho_total += int(arquivo['size'])
+                    quantidade_arquivos += 1
+                    arquivo['current_path'] = f"{current_path}{arquivo['name']}"
+                    lista_arquivos.append(arquivo)
+            else:
+                quantidade_arquivos_temp, tamanho_total_temp, lista_arquivos_temp = obter_dados_pasta(
+                    servico,
+                    team_drive_id,
+                    arquivo['id'],
+                    current_path = f"{current_path}{arquivo['name']}/")
+                quantidade_arquivos += quantidade_arquivos_temp
+                tamanho_total += tamanho_total_temp
+                lista_arquivos.extend(lista_arquivos_temp)
+
+        # Verificar se há mais páginas
+        pagina_token = resultados.get('nextPageToken')
+        if not pagina_token:
+            break
+
+    return quantidade_arquivos, tamanho_total, lista_arquivos
+
+
+# Escopos necessários para acessar a API do Google Drive
+escopos = ['https://www.googleapis.com/auth/drive.readonly']
+
+# Autenticar o serviço
+gd_service = autenticar_servico(google_drive_json_filename, escopos)
+
+# Obter dados da pasta no Team Drive
+quantidade_arquivos, tamanho_total, lista_arquivos = obter_dados_pasta(gd_service, team_drive_id, dest_folder_id)
+
+# Exibir resultados
+print(f'Quantidade de arquivos na pasta: {quantidade_arquivos}')
+print(f'Tamanho total ocupado: {tamanho_total} bytes')
 
 
