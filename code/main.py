449a8734f27d75d65582ad92f286ddef68531818
Heber Scachetti <heber.augusto@gmail.com>
2024-01-08 22:11:34 -0300

    Create main.py

---
+++ code/main.py
@@ -0,0 +1,4 @@
+from google.cloud import storage
+
+google_storage_json_filename = os.getenv('GOOGLE_STORAGE_CREDENTIALS', '')
+google_drive_json_filename = os.getenv('GOOGLE_DRIVE_CREDENTIALS', '')
