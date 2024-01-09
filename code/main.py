48a78c0b75d2a6104281201f90e66bf0888ff0aa
Heber Scachetti <heber.augusto@gmail.com>
2024-01-09 00:00:57 -0300

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,4 +1,29 @@
 from google.cloud import storage
+import os
 
-google_storage_json_filename = os.getenv('GOOGLE_STORAGE_CREDENTIALS', '')
-google_drive_json_filename = os.getenv('GOOGLE_DRIVE_CREDENTIALS', '')
+google_storage_json_filename = os.getenv('GOOGLE_APPLICATION_CREDENTIALS', '')
+google_drive_json_filename = os.getenv('GD_JSON_FILE', '')
+
+# Cliente do Google Cloud Storage
+storage_client = storage.Client.from_service_account_json(json_filename)
+bucket_id = os.getenv('GCP_BUCKET', '')
+
+gs_prefix = os.getenv('GCP_PREFIX', '')
+
+
+bucket_id = 'observatorio-oncologia'
+
+# Acesse o bucket desejado
+blobs = storage_client.list_blobs(
+        bucket_or_name = bucket_id,
+        prefix = gs_prefix)
+gs_files = [blob for blob in blobs]
+
+gs_files_list = [\
+  {
+      'current_path': gs_file.name.replace(gs_prefix, ''),
+      'size': int(gs_file.size)
+  } \
+  for gs_file in gs_files if gs_file.size > 0]
+
+print(f"{gs_prefix} : {sum([file['size'] for file in gs_files_list])} bytes")
