02f63660ea3e1a12f82f7b4889107304edbf10bf
Heber Scachetti <heber.augusto@gmail.com>
2024-01-09 00:10:38 -0300

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,11 +1,13 @@
-from google.cloud import storage
 import os
+from google.cloud import storage
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
 
 google_storage_json_filename = os.getenv('GOOGLE_APPLICATION_CREDENTIALS', '')
 google_drive_json_filename = os.getenv('GD_JSON_FILE', '')
 
 # Cliente do Google Cloud Storage
-storage_client = storage.Client.from_service_account_json(json_filename)
+storage_client = storage.Client.from_service_account_json(google_storage_json_filename)
 bucket_id = os.getenv('GCP_BUCKET', '')
 
 gs_prefix = os.getenv('GCP_PREFIX', '')
@@ -27,3 +29,13 @@ gs_files_list = [\
   for gs_file in gs_files if gs_file.size > 0]
 
 print(f"{gs_prefix} : {sum([file['size'] for file in gs_files_list])} bytes")
+
+
+
+
+def autenticar_servico(json_caminho, escopos):
+    credenciais = service_account.Credentials.from_service_account_file(json_caminho, scopes=escopos)
+    return build('drive', 'v3', credentials=credenciais)
+
+
+
